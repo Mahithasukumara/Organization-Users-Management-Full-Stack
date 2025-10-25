be6bd75494c4605e73947aaf57dc969e5dc41e92
Mahithasukumara <mahissk2005@gmail.com>
2025-10-25 23:11:06 +0530

    second commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express, { json } from "express";
 import orgRouter from "./routers/org.route.js";
 import userRouter from "./routers/user.route.js";
+import dotenv from "dotenv";
+dotenv.config(); 
 const app = express();
 app.use(json());
 app.use("/org", orgRouter);
 app.use("/user", userRouter);
-app.listen(3010, () => {
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
     console.log("server is up and running");
 })
